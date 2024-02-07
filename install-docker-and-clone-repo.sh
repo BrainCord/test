4c997692e07b1002f2976034400dd0a40777852f
OWL <60858072+BrainCord@users.noreply.github.com>
2024-02-07 11:29:43 +0300

    Add files via upload

---
+++ install-docker-and-clone-repo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Обновление списка пакетов
+sudo apt-get update
+
+# Установка необходимых пакетов
+sudo apt-get install -y ca-certificates curl gnupg
+
+# Создание директории для ключей apt
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Добавление официального GPG ключа Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Настройка прав доступа для ключа
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Добавление репозитория Docker в список источников пакетов
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Обновление списка пакетов после добавления нового репозитория
+sudo apt-get update
+
+# Установка Docker Engine, CLI и плагинов
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Запуск тестового контейнера Docker
+sudo docker run hello-world
+
+# Клонирование репозитория simple-taiko-node
+git clone https://github.com/taikoxyz/simple-taiko-node.git
+
+# Вывод сообщения о завершении
+echo "Node succesfully installed, make configuration in /root/simple-taiko-node/ | Files: .ENV И .ENV.SAMPLE!"
