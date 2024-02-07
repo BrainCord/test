cd72146e315b22bbed3f4cdfc7e6d06b5b52a9aa
OWL <60858072+BrainCord@users.noreply.github.com>
2024-02-07 13:46:55 +0300

    Update install-docker-and-clone-repo.sh

--- install-docker-and-clone-repo.sh
+++ install-docker-and-clone-repo.sh
@@ -36,25 +36,3 @@ git clone https://github.com/taikoxyz/simple-taiko-node.git
 # Переход в каталог проекта
 cd simple-taiko-node
 
-# Запрос данных у пользователя
-echo "Введите значения для конфигурации:"
-read -p "L1_ENDPOINT_HTTP: " L1_ENDPOINT_HTTP
-read -p "L1_ENDPOINT_WS: " L1_ENDPOINT_WS
-read -p "ENABLE_PROPOSER (true/false): " ENABLE_PROPOSER
-read -p "L1_PROPOSER_PRIVATE_KEY: " L1_PROPOSER_PRIVATE_KEY
-read -p "PROVER_ENDPOINTS: " PROVER_ENDPOINTS
-
-# Обновление .env.sample с введенными значениями
-cat > .env.sample <<EOF
-L1_ENDPOINT_HTTP=$L1_ENDPOINT_HTTP
-L1_ENDPOINT_WS=$L1_ENDPOINT_WS
-ENABLE_PROPOSER=$ENABLE_PROPOSER
-L1_PROPOSER_PRIVATE_KEY=$L1_PROPOSER_PRIVATE_KEY
-PROVER_ENDPOINTS=$PROVER_ENDPOINTS
-EOF
-
-# Создание дубликата файла под названием .env
-cp .env.sample .env
-
-# Вывод сообщения о завершении
-echo "Конфигурация завершена и сохранена в .env.sample и .env"
